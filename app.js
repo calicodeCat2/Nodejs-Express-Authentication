062fe8342996242bc26739d23b9e1a906accda93
David Murcek <dfmurcek@gmail.com>
2019-04-25 13:48:21 -0700

    Completed express bcrypt authentication model with MySQL db.

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+
+// Authentication packages
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var MySQLStore = require('express-mysql-session')(session);
+var bcrypt = require('bcrypt');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,11 +29,64 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator()); // this line must be immediately after any of the bodyParser middlewares!
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var options = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database : process.env.DB_NAME,
+  socketPath: '/tmp/mysql.sock'
+};
+
+var sessionStore = new MySQLStore(options);
+
+app.use(session({
+  secret: 'wombatwhisperer',
+  resave: false,
+  store: sessionStore,
+  saveUninitialized: false,
+  // cookie: { secure: true }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
 app.use('/', index);
 app.use('/users', users);
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    console.log(username);
+    console.log(password);
+    const db = require('./db')
+
+    db.query('SELECT id, password FROM users WHERE username = ?', [username], function(err, results, fields) {
+      if (err) {done(err)};
+
+      if (results.length === 0) {
+        return done(null, false);
+      } else {
+      
+      const hash = results[0].password.toString();
+      bcrypt.compare(password, hash, function(err, response) {
+        if (response === true) {
+          return done(null, {user_id: results[0].id})
+        } else {
+          return done(null, false)
+        }
+      })
+    }
+    })
+  }
+));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
